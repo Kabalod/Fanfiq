89ebef6137a7681b4d7650edb8ca9d6733d62df6
Kabalod <kbalodk@gmail.com>
2025-09-02 13:18:51 +0300

    055: test(parsers): add test structure for ficbook parser

---
+++ parsers/ficbook/tests/test_parser.py
@@ -0,0 +1,8 @@
+import pytest
+from ..parser import FicbookParser
+
+def test_ficbook_parser():
+    # TODO: Add a mock HTML file and test parsing logic
+    parser = FicbookParser()
+    # assert parser.parse("mock_url") == expected_data
+    assert True
